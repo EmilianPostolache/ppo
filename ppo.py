51396f2455139ad17670db479cd7ff6fc578a21f
emilian <emilianpos@gmail.com>
2019-04-03 13:58:29 +0200

    modified ValueFunction

--- ppo.py
+++ ppo.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.signal import lfilter
 import tensorflow as tf
+from sklearn.utils import shuffle
 
 
 class PPO:
@@ -139,66 +140,166 @@ class Policy:
 
 
 class ValueFunction:
-    """Value Function network"""
-    LAYER_MULT = 5
-    EPOCHS = 10
+    """ NN-based state-value function """
+    HID1_MULT = 10
 
-    def __init__(self, dim_obs, lr, logger):
-        self.dim_obs = dim_obs
-        self.lr = lr
-        self.logger = logger
+    def __init__(self, obs_dim):
+        """
+        Args:
+            obs_dim: number of dimensions in observation vector (int)
+            hid1_mult: size of first hidden layer, multiplier of obs_dim
+        """
+        self.replay_buffer_x = None
+        self.replay_buffer_y = None
+        self.obs_dim = obs_dim
+        self.hid1_mult = self.HID1_MULT
+        self.epochs = 10
+        self.lr = None  # learning rate set in _build_graph()
         self._build_graph()
-        self._initialize()
-
-    def _build_graph(self):
-        self.graph = tf.Graph()
-        with self.graph.as_default():
-            self._placeholders()
-            self._layers()
-            self._loss()
-            self.init = tf.global_variables_initializer()
-
-    def _placeholders(self):
-        self.ph_obs = tf.placeholder(tf.float32, (None, self.dim_obs), 'obs_valf')
-        self.ph_val = tf.placeholder(tf.float32, (None,), 'val_valf')
-
-    def _layers(self):
-        # layer 1
-        out = tf.layers.dense(self.ph_obs, 64, tf.nn.tanh,  # self.dim_obs * self.LAYER_MULT,
-                              kernel_initializer=tf.random_normal_initializer(stddev=np.sqrt(1 / self.dim_obs)),
-                              name='layer1')
-        # layer 2
-        out = tf.layers.dense(out, 64, tf.nn.tanh,  # self.dim_obs * self.LAYER_MULT,
-                              kernel_initializer=tf.random_normal_initializer(stddev=np.sqrt(1 / 64)),
-                              name='layer2')
-        # layer 3
-        #  out = tf.layers.dense(out, int(self.dim_obs * self.LAYER_MULT / 2), tf.nn.tanh, name='layer3')
-        # output layer (mean)
-        out = tf.layers.dense(out, 1, name='means')
-        self.out = tf.squeeze(out)
-
-    def _loss(self):
-        self.loss = tf.reduce_mean(tf.square(self.out - self.ph_val))
-        optimizer = tf.train.AdamOptimizer(self.lr)
-        self.op_train = optimizer.minimize(self.loss)
-
-    def _initialize(self):
-        self.sess = tf.Session(graph=self.graph)
+        self.sess = tf.Session(graph=self.g)
         self.sess.run(self.init)
 
-    def close(self):
+    def _build_graph(self):
+        """ Construct TensorFlow graph, including loss function, init op and train op """
+        self.g = tf.Graph()
+        with self.g.as_default():
+            self.obs_ph = tf.placeholder(tf.float32, (None, self.obs_dim), 'obs_valfunc')
+            self.val_ph = tf.placeholder(tf.float32, (None,), 'val_valfunc')
+            # hid1 layer size is 10x obs_dim, hid3 size is 10, and hid2 is geometric mean
+            hid1_size = self.obs_dim * self.hid1_mult  # default multipler 10 chosen empirically on 'Hopper-v1'
+            hid3_size = 5  # 5 chosen empirically on 'Hopper-v1'
+            hid2_size = int(np.sqrt(hid1_size * hid3_size))
+            # heuristic to set learning rate based on NN size (tuned on 'Hopper-v1')
+            self.lr = 1e-2 / np.sqrt(hid2_size)  # 1e-3 empirically determined
+            print('Value Params -- h1: {}, h2: {}, h3: {}, lr: {:.3g}'
+                  .format(hid1_size, hid2_size, hid3_size, self.lr))
+            # 3 hidden layers with tanh activations
+            out = tf.layers.dense(self.obs_ph, hid1_size, tf.tanh,
+                                  kernel_initializer=tf.random_normal_initializer(
+                                      stddev=np.sqrt(1 / self.obs_dim)), name="h1")
+            out = tf.layers.dense(out, hid2_size, tf.tanh,
+                                  kernel_initializer=tf.random_normal_initializer(
+                                      stddev=np.sqrt(1 / hid1_size)), name="h2")
+            out = tf.layers.dense(out, hid3_size, tf.tanh,
+                                  kernel_initializer=tf.random_normal_initializer(
+                                      stddev=np.sqrt(1 / hid2_size)), name="h3")
+            out = tf.layers.dense(out, 1,
+                                  kernel_initializer=tf.random_normal_initializer(
+                                      stddev=np.sqrt(1 / hid3_size)), name='output')
+            self.out = tf.squeeze(out)
+            self.loss = tf.reduce_mean(tf.square(self.out - self.val_ph))  # squared loss
+            optimizer = tf.train.AdamOptimizer(self.lr)
+            self.train_op = optimizer.minimize(self.loss)
+            self.init = tf.global_variables_initializer()
+        self.sess = tf.Session(graph=self.g)
+        self.sess.run(self.init)
+
+    def fit(self, x, y, logger):
+        """ Fit model to current data batch + previous data batch
+
+        Args:
+            x: features
+            y: target
+            logger: logger to save training loss and % explained variance
+        """
+        num_batches = max(x.shape[0] // 256, 1)
+        batch_size = x.shape[0] // num_batches
+        y_hat = self.predict(x)  # check explained variance prior to update
+        old_exp_var = 1 - np.var(y - y_hat) / np.var(y)
+        if self.replay_buffer_x is None:
+            x_train, y_train = x, y
+        else:
+            x_train = np.concatenate([x, self.replay_buffer_x])
+            y_train = np.concatenate([y, self.replay_buffer_y])
+        self.replay_buffer_x = x
+        self.replay_buffer_y = y
+        for e in range(self.epochs):
+            x_train, y_train = shuffle(x_train, y_train)
+            for j in range(num_batches):
+                start = j * batch_size
+                end = (j + 1) * batch_size
+                feed_dict = {self.obs_ph: x_train[start:end, :],
+                             self.val_ph: y_train[start:end]}
+                _, l = self.sess.run([self.train_op, self.loss], feed_dict=feed_dict)
+        y_hat = self.predict(x)
+        loss = np.mean(np.square(y_hat - y))  # explained variance after update
+        exp_var = 1 - np.var(y - y_hat) / np.var(y)  # diagnose over-fitting of val func
+
+        logger.log({'ValFuncLoss': loss,
+                    'ExplainedVarNew': exp_var,
+                    'ExplainedVarOld': old_exp_var})
+
+    def predict(self, x):
+        """ Predict method """
+        feed_dict = {self.obs_ph: x}
+        y_hat = self.sess.run(self.out, feed_dict=feed_dict)
+
+        return np.squeeze(y_hat)
+
+    def close_sess(self):
+        """ Close TensorFlow session """
         self.sess.close()
 
-    def update(self, obs, returns):
-        loss = None
-        for e in range(self.EPOCHS):
-            feed_dict = {self.ph_obs: obs,
-                         self.ph_val: returns}
-            self.sess.run(self.op_train, feed_dict)
-            loss = self.sess.run(self.loss, feed_dict)
-        self.logger.log({'ValFunctionLoss': loss})
-
-    def predict(self, obs):
-        feed_dict = {self.ph_obs: obs}
-        pred = self.sess.run(self.out, feed_dict)
-        return np.squeeze(pred)
+    # """Value Function network"""
+    # LAYER_MULT = 5
+    # EPOCHS = 10
+    #
+    # def __init__(self, dim_obs, lr, logger):
+    #     self.dim_obs = dim_obs
+    #     self.lr = lr
+    #     self.logger = logger
+    #     self._build_graph()
+    #     self._initialize()
+    #
+    # def _build_graph(self):
+    #     self.graph = tf.Graph()
+    #     with self.graph.as_default():
+    #         self._placeholders()
+    #         self._layers()
+    #         self._loss()
+    #         self.init = tf.global_variables_initializer()
+    #
+    # def _placeholders(self):
+    #     self.ph_obs = tf.placeholder(tf.float32, (None, self.dim_obs), 'obs_valf')
+    #     self.ph_val = tf.placeholder(tf.float32, (None,), 'val_valf')
+    #
+    # def _layers(self):
+    #     # layer 1
+    #     out = tf.layers.dense(self.ph_obs, 64, tf.nn.tanh,  # self.dim_obs * self.LAYER_MULT,
+    #                           kernel_initializer=tf.random_normal_initializer(stddev=np.sqrt(1 / self.dim_obs)),
+    #                           name='layer1')
+    #     # layer 2
+    #     out = tf.layers.dense(out, 64, tf.nn.tanh,  # self.dim_obs * self.LAYER_MULT,
+    #                           kernel_initializer=tf.random_normal_initializer(stddev=np.sqrt(1 / 64)),
+    #                           name='layer2')
+    #     # layer 3
+    #     #  out = tf.layers.dense(out, int(self.dim_obs * self.LAYER_MULT / 2), tf.nn.tanh, name='layer3')
+    #     # output layer (mean)
+    #     out = tf.layers.dense(out, 1, name='means')
+    #     self.out = tf.squeeze(out)
+    #
+    # def _loss(self):
+    #     self.loss = tf.reduce_mean(tf.square(self.out - self.ph_val))
+    #     optimizer = tf.train.AdamOptimizer(self.lr)
+    #     self.op_train = optimizer.minimize(self.loss)
+    #
+    # def _initialize(self):
+    #     self.sess = tf.Session(graph=self.graph)
+    #     self.sess.run(self.init)
+    #
+    # def close(self):
+    #     self.sess.close()
+    #
+    # def update(self, obs, returns):
+    #     loss = None
+    #     for e in range(self.EPOCHS):
+    #         feed_dict = {self.ph_obs: obs,
+    #                      self.ph_val: returns}
+    #         self.sess.run(self.op_train, feed_dict)
+    #         loss = self.sess.run(self.loss, feed_dict)
+    #     self.logger.log({'ValFunctionLoss': loss})
+    #
+    # def predict(self, obs):
+    #     feed_dict = {self.ph_obs: obs}
+    #     pred = self.sess.run(self.out, feed_dict)
+    #     return np.squeeze(pred)
